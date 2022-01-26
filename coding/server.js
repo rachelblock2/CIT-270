15975cfcf20df60c89334a0d3e3f0211d0794d46
rachelblock2 <36903263+rachelblock2@users.noreply.github.com>
2022-01-26 13:44:23 -0700

    Created successful req/res with username/password

--- coding/server.js
+++ coding/server.js
@@ -1,11 +1,22 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const port = 3000;
 
 const app = express();
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.send("Hello Browser!");
 });
+
+app.post('/login', (req, res) => {
+    console.log(JSON.stringify(req.body));
+    if (req.body.userName ==="rachellofgran" && req.body.password === "rachie10!") {
+        res.send("Welcome!");
+    } else {
+        res.send("Who are you?");
+    }
+})
 
 app.listen(port, () => {});
