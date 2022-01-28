31fc53b0241e455871350624283a03b6b9e228fe
rachelblock2 <36903263+rachelblock2@users.noreply.github.com>
2022-01-28 13:14:14 -0700

    Added notes

--- coding/server.js
+++ coding/server.js
@@ -10,9 +10,9 @@ app.get('/', (req, res) => {
     res.send("Hello Browser!");
 });
 
-app.post('/login', (req, res) => {
-    console.log(JSON.stringify(req.body));
-    if (req.body.userName ==="rachellofgran" && req.body.password === "rachie10!") {
+app.post('/login', (req, res) => { //Goes to page off of localhost
+    console.log(JSON.stringify(req.body)); //reads json req from loginscript
+    if (req.body.userName ==="rachellofgran" && req.body.password === "rachie10!") { //checks username and password
         res.send("Welcome!");
     } else {
         res.send("Who are you?");
@@ -23,3 +23,5 @@ app.listen(port, () => {});
 
 // Authorization (auth) = how many permissions you have, ex. Adminstrator privileges
 // Authentication (auth Z)= proving who you are
+// npm start
+// loginscript.bat
